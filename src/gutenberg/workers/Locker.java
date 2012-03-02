6bb804a0f5405132ec2755389c68c848461a2b66
Akshay Damarla <akshay.damarla@gmail.com>
2012-03-02 17:21:37 +0530

    - Locker.java now reads rotation info for scans and sets them upright using convert

--- src/gutenberg/workers/Locker.java
+++ src/gutenberg/workers/Locker.java
@@ -6,6 +6,7 @@ import gutenberg.blocs.ManifestType;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Locker {
@@ -28,48 +29,51 @@ public class Locker {
 	}
 
 	public ManifestType save(File[] scans) throws Exception {
+		
 		ManifestType manifest = new ManifestType();
 		manifest.setRoot(LOCKER);
-		Path locker = new File(LOCKER).toPath();
+		
+		Path locker = new File(LOCKER).toPath();		
+		String scanId = null; 
+		boolean flip = false; 
+		String[] tokens = null;
+		
 		for (File scan:scans) {
-			if (locker.resolve(scan.getName()).toFile().exists()) {
-				if (!scan.delete()) {
-					throw new Exception("Received scan " 
-							+ scan.getName() + " could not be deleted");
+			
+			tokens = scan.getName().split("\\.");
+			scanId = tokens[0];
+			flip = tokens[1].equals("1") ? true:false;
+			if (!Files.exists(locker.resolve(scanId))) {						
+				if (convert(scan.toPath(), locker.resolve(scanId), SCAN_SIZE, flip) != 0 ||
+					convert(scan.toPath(), locker.resolve("thumb-" + scanId), THUMB_SIZE, flip) != 0) {
+					throw new Exception("Error running convert utility on "
+							+ scan.getName());
 				}
-				continue;
-			}
-						
-			if (convert(scan.getPath(),
-					locker.resolve(scan.getName()).toString(), SCAN_SIZE) == 0 &&
-				convert(scan.getPath(), 
-					locker.resolve("thumb-" + scan.getName()).toString(), THUMB_SIZE) == 0) {
-				if (!scan.delete()) {
-					throw new Exception("Received scan " 
-							+ scan.getName() + " could not be deleted");
-				}
-				
-			} else {
-				throw new Exception("Error resizing scan "
-						+ scan.getName());
+
+				EntryType image = new EntryType();
+				image.setId(scanId);
+				manifest.addImage(image);
 			}
 			
-			EntryType image = new EntryType();
-			image.setId(scan.getName());
-			manifest.addImage(image);
+			if (!scan.delete()) {
+				throw new Exception("SaveScan error: could not delete" + scan.getName());
+			}
+			
 		}
 		return manifest;
 	}
 
 	private String LOCKER;
 	
-	private int convert(String src, String target, String size)
+	private int convert(Path src, Path target, String size, boolean flip)
 			throws Exception {
-		ProcessBuilder pb = 
-				new ProcessBuilder("convert", src, "-resize", size, target);
-		System.out.println("[receiveScan]: convert " + src + " -resize " + size +
-				" " + target);
-
+		ProcessBuilder pb = new ProcessBuilder();
+		if (flip) {
+			pb.command("convert", src.toString(), "-flip", "-resize", size, target.toString());
+		} else {
+			pb.command("convert", src.toString(), "-resize", size, target.toString());
+		}
+		
 		pb.directory(new File(LOCKER));
 		pb.redirectErrorStream(true);
 
