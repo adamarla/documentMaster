28db664e1e2506969a4b946dcce8205087c705ca
Akshay Damarla <akshay.damarla@gmail.com>
2015-05-05 12:21:24 +0530

    include question id and sha1sum in bundle manifest

--- src/gutenberg/workers/Bundles.java
+++ src/gutenberg/workers/Bundles.java
@@ -2,7 +2,8 @@ package gutenberg.workers;
 
 import gutenberg.blocs.ManifestType;
 
-import java.io.FileWriter;
+import java.security.MessageDigest;
+import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
@@ -55,38 +56,36 @@ public class Bundles {
             Path zipManifest = vaultPath.resolve(manifestXML);
             PrintWriter pw = new PrintWriter(zipManifest.toFile());
             
+            // Copy existing manifest entries (if any) OR start new one
             if (Files.exists(root.resolve(manifestXML))) {
                 List<String> lines = Files.readAllLines(root.resolve(manifestXML), 
                     StandardCharsets.UTF_8);
-                // Copy existing manifest
                 pw.println(lines.get(0));
                 boolean skip = false;
                 for (int i = 1; i < lines.size()-1; i++) {
                     for (String question : questions) {
-                        if (lines.get(i).contains(question.split("\\|")[0])) {
+                        if (lines.get(i).contains(question.split("\\|")[1])) {
                             skip = true;
                             break;
                         }
                     }
-                    if (!skip)
-                        pw.println(lines.get(i));
+                    if (!skip) pw.println(lines.get(i));
                 }
             } else {
-                // Start new manifest
-                pw.println("<qbundle tag=\"" + bundleId + "\">");
+                pw.println(String.format("<qbundle tag=\"%s\">", bundleId));
             }
             
             Path srcDir, destDir = null;
             for (String question : questions) {
-                String path = question.split("\\|")[0], label = question.split("\\|")[1];
+                String[] tokens = question.split("\\|");
+                String id=tokens[0], path = tokens[1], label = tokens[2];
                 
                 // Create question dir
                 destDir = fs.getPath(root.toString(), path.replace('/', '-'));
                 if (Files.notExists(destDir)) Files.createDirectory(destDir);
                 
                 srcDir = vaultPath.resolve(path);
-                try (DirectoryStream<Path> stream = 
-                    Files.newDirectoryStream(srcDir, "*.svg")) {
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(srcDir, "*.svg")) {
                     for (Path entry: stream) {
                         Files.copy(entry, destDir.resolve(entry.getFileName().toString()), 
                             StandardCopyOption.REPLACE_EXISTING);
@@ -94,14 +93,15 @@ public class Bundles {
                     Files.copy(srcDir.resolve(QSN_XML), destDir.resolve(QSN_XML), 
                         StandardCopyOption.REPLACE_EXISTING);
                 }
-                pw.println(String.format("    <question tag=\"%s\" label=\"%s\"/>", path, label));
+                
+                String signature = getSHA1Sum(srcDir.resolve(QSN_XML));
+                pw.println(String.format(QSN_TAG, path, label, id, signature));
                 System.out.println("Processed " + srcDir.resolve(QSN_XML).toString());
             }
             pw.println("</qbundle>");
             pw.close();
             
-            Files.copy(zipManifest, root.resolve(manifestXML),
-                StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(zipManifest, root.resolve(manifestXML), StandardCopyOption.REPLACE_EXISTING);
             Files.delete(zipManifest);
         }
         ManifestType manifest = new ManifestType();
@@ -110,11 +110,28 @@ public class Bundles {
         return manifest;
     }
     
-    public void removeFrom(String bundleId, String[] questionIds) {
+    private String getSHA1Sum(Path path) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+        int bytesRead = 0;
+        byte[] byteBuf = new byte[1024];
+        try (java.io.InputStream is = new FileInputStream(path.toFile())) {
+            while ((bytesRead = is.read(byteBuf)) != -1) {
+                md.update(byteBuf, 0, bytesRead);
+            }
+        }
         
+        byte[] SHA1digest = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (byte b : SHA1digest){
+            sb.append(String.format("%02x", b));
+        }
+        
+        return sb.toString().substring(0, 12);
     }
     
     Path vaultPath, bundlePath;
     final String QSN_XML = "question.xml";
+    final String QSN_TAG = "    <question tag=\"%s\" label=\"%s\" id=\"%s\" signature=\"\"/>";
 
 }
